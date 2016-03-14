d6d410fd24f118577fe8e70791a374dd78076d70
Andrii Plotnikov <plotnikovandrey1990@gmail.com>
2016-03-14 20:43:25 +0100

    Add Guice module

---
+++ src/main/java/org/home/guice/ApplicationModule.java
@@ -0,0 +1,10 @@
+package org.home.guice;
+
+import com.google.inject.AbstractModule;
+
+public class ApplicationModule extends AbstractModule {
+    @Override
+    protected void configure() {
+
+    }
+}
